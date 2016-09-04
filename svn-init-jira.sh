a6ef5eb6ff5bde501eb1648c9e7e55a18c341f50
Takeshi Nakamura <taqueci.n@gmail.com>
2016-09-05 07:35:19 +0900

    Add scripts for Atlassian Crowd

--- svn-init-jira.sh
+++ svn-init-jira.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-AUTHZ=/var/lib/svn/conf/authz
 TMPL=/var/lib/svn/conf/authz.tmpl
+
+OWNER=apache
+GROUP=apache
 
 dir=`dirname $0`
 
+tmp_tmpl=`mktemp`
+
 $dir/svn-init.sh $* || exit 1
 
-perl $dir/svn-authz-tmpl.pl --verbose -t $TMPL -o $TMPL $* || exit 1
+perl $dir/svn-authz-tmpl.pl --verbose -t $TMPL -o $tmp_tmpl $* &&
+	install -o $OWNER -g $GROUP -m 600 $tmp_tmpl $TMPL || exit 1
+
+rm -f $tmp_tmpl
+
+$dir/svn-authz-crowd.sh || exit 1
